fc44604c518a9c2f2bb81881bc9533a4672f2890
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-09-28 22:45:05 +0100

    Added guarding for invalid digits in base

--- all-your-base/all-your-base.lisp
+++ all-your-base/all-your-base.lisp
@@ -5,7 +5,8 @@
 (in-package :all-your-base)
 
 (defun rebase (list-digits in-base out-base)
-  (from-base-10-to-any (to-base-10 list-digits in-base) out-base))
+  (unless (or (<= in-base 1) (>= in-base 37)(<= out-base 1) (>= out-base 37) (not (are-digits-valid-for-base list-digits in-base)))
+    (from-base-10-to-any (to-base-10 list-digits in-base) out-base)))
 
 (defun to-base-10 (list-digits in-base)
   (let ((final-number 0)
@@ -24,3 +25,11 @@
           (setq original-number (floor (/ original-number target-base)))
           (setq base-num-list (cons moded base-num-list)))
     base-num-list))
+
+(defun are-digits-valid-for-base (list-digits base)
+  (loop for list-item in list-digits
+        always (<= list-item (- base 1))))
+
+
+;(format t "~A~%" (rebase '(1 0 1 0 1 0) 2 1))
+;(format t "~A~%" (are-digits-valid-for-base '(1 1 1 0 1 0) 2))
