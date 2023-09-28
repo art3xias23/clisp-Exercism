66f3220c849f73a8aac5480a2571a32ddd684edb
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-09-28 19:33:08 +0100

    Fixed issued by adding equality type to member and remove functions

--- allergies/allergies.lisp
+++ allergies/allergies.lisp
@@ -6,7 +6,8 @@
 (in-package :allergies)
 
 (defun allergic-to-p (score allergen)
-  (not (null (member allergen (list score)))))
+ (let ((allergen-list (list score)))
+   (member allergen allergen-list :test #'string=)))
   
 
 (defun list (score)
@@ -16,18 +17,25 @@
     (setq num score)
     (loop 
      (if (<= num 0)
-        (return my-allergen-list)
+        (return  my-allergen-list)
         (progn
               (setq log2num (floor (get-log2-value num)))
+              (format t "log2: ~D~%" log2num)
               (setq my-allergen-list (add-item-to-list (cdr (assoc log2num (get-allergy-list))) my-allergen-list))
               (setq num (- num (expt 2 log2num))))))))
 
-(defun add-item-to-list (item list)
-  (format t "Adding item: ~A~%" item)
-  (when (and (not (null item)) (null (member item list)))
-    (format t "Added item: ~A~%" item)
-    (setq list (cons item list)))
-  list)
+(defun add-item-to-list (item lst)
+  (when (not (null item))
+    (let ((var "")
+          (original-list lst))
+      (setq var (member item original-list :test #'string=))
+      (if (null var)
+          (setq original-list (cons item original-list))
+          (progn 
+            (setq original-list (remove item original-list :test #'string=))
+            (setq original-list (cons item original-list))))
+      (format t "Final list: ~A~%" original-list)
+      original-list)))
 
 (defun get-log2-value (initial-number)
   (/ (log initial-number) (log 2)))
@@ -45,5 +53,5 @@
         (9 . "eggs")
         (10 . "eggs")))
 
-(format t "Answer: ~A~%" (list 257))
-
+;(format t "A: ~A~%" (allergic-to-p 255 "cats"))
+(list 509)
