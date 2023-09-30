d4039beae4c79c65e7f71a5ec56ffd457a2c3f2f
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-09-30 18:45:05 +0100

    wip on binary search

--- binary-search/binary-search.lisp
+++ binary-search/binary-search.lisp
@@ -6,10 +6,26 @@
 
 (defun binary-find (arr el)
   (let 
-      ((middle-pos (round (/ (length arr) 2)))
+      ((middle-pos (1- (ceiling (/ (length arr) 2))))
        (middle-num 0))
+    (format t "MP: ~D~%" middle-pos)
+    (format t "Mn: ~D~%" middle-num)
+    (format t "Arr: ~A~%" arr)
     (setq middle-num (aref arr middle-pos))
-    (if (= middle-num el)
-        el
-        (binary-find (subseq arr middle-pos)))))
+    (if (>= middle-pos 0)
+        (cond
+            ((= middle-num el) middle-pos)
+            ((> middle-num el) (binary-find (subseq arr 0 (1- middle-pos) el)))
+            ((< middle-num el) (binary-find (subseq arr (1+ middle-pos) el))))
+        NIL)))
+
+(format t "pos: ~D~%" (binary-find #(6) 6))
+(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 6))
+(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 1))
+(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 11))
+(format t "pos: ~D~%" (binary-find #(1 3 5 8 13 21 34 55 89 144 233 377 634) 144))
+(format t "pos: ~D~%" (binary-find #(1 3 5 8 13 21 34 55 89 144 233 377)))
+(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 7))
+(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 0))
+(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 13))
   
