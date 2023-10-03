83118644a9df118318f02a4e9ae219b8d4d2d8cc
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-10-03 23:50:27 +0100

    Getting all positive num results and handled 0 initial input

--- collatz-conjecture/collatz-conjecture.lisp
+++ collatz-conjecture/collatz-conjecture.lisp
@@ -4,4 +4,14 @@
 
 (in-package :collatz-conjecture)
 
-(defun collatz (n))
+(defun collatz (n)
+  (format t "Num is: ~D~%" n)
+  (if (null n)
+      (return-from collatz NIL))
+  (when (= n 1)
+    (return-from collatz 0))
+  (if (= (mod n 2) 0)
+    (1+ (collatz (/ n 2)))
+    (1+ (collatz (1+ (* 3 n))))))
+
+(format t "endresult is: ~D~%" (collatz 12))
