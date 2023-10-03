ca35bfebcee7595fe892eecb5c5c3f62fefe9454
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-10-03 23:03:53 +0100

    Added an endresult for the total position number of the element

--- binary-search/binary-search.lisp
+++ binary-search/binary-search.lisp
@@ -6,26 +6,35 @@
 
 (defun binary-find (arr el)
   (let 
-      ((middle-pos (1- (ceiling (/ (length arr) 2))))
+      ((middle-pos (floor (/ (length arr) 2))) 
        (middle-num 0))
-    (format t "MP: ~D~%" middle-pos)
-    (format t "Mn: ~D~%" middle-num)
-    (format t "Arr: ~A~%" arr)
+    (format t "Middle Pos: ~D~%" middle-pos)
     (setq middle-num (aref arr middle-pos))
-    (if (>= middle-pos 0)
+    (format t "Mid Num: ~D~%" middle-num)
+    (format t "Arr: ~A~%" arr)
+    (if (= (length arr) 0)
+        NIL
         (cond
-            ((= middle-num el) middle-pos)
-            ((> middle-num el) (binary-find (subseq arr 0 (1- middle-pos) el)))
-            ((< middle-num el) (binary-find (subseq arr (1+ middle-pos) el))))
-        NIL)))
+            ((= middle-num el) 
+             (progn 
+               (format t "got to middle pos")
+               middle-pos))
+            ((> middle-num el) 
+             (let ((result (binary-find (subseq arr 0  middle-pos) el)))
+               (if result
+                   (+ (1+ result) middle-pos))))
+            ((< middle-num el)
+             (let ((result (binary-find (subseq arr (1+ middle-pos)) el)))
+               (if result
+                   (+ (1+ result) middle-pos))))))))
 
-(format t "pos: ~D~%" (binary-find #(6) 6))
-(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 6))
-(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 1))
-(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 11))
-(format t "pos: ~D~%" (binary-find #(1 3 5 8 13 21 34 55 89 144 233 377 634) 144))
-(format t "pos: ~D~%" (binary-find #(1 3 5 8 13 21 34 55 89 144 233 377)))
-(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 7))
-(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 0))
-(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 13))
+; (format t "pos: ~D~%" (binary-find #(6) 6))
+; (format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 6))
+ (format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 1))
+;(format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 11))
+; (format t "pos: ~D~%" (binary-find #(1 3 5 8 13 21 34 55 89 144 233 377 634) 144))
+; (format t "pos: ~D~%" (binary-find #(1 3 5 8 13 21 34 55 89 144 233 377)))
+; (format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 7))
+; (format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 0))
+; (format t "pos: ~D~%" (binary-find #(1 3 4 6 8 9 11) 13))
   
