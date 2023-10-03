6ff811897fdf1ae1d8af20b00b7c34c32b2c694f
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-10-04 00:12:06 +0100

    Provided solution for sum of squares

--- difference-of-squares/difference-of-squares.lisp
+++ difference-of-squares/difference-of-squares.lisp
@@ -8,12 +8,17 @@
 
 (defun square-of-sum (n)
   "Calculates the square of the sum for a given number."
-  )
+  (expt (reduce #'+ (loop for i from 0 to n collect i)) 2))
+  
 
 (defun sum-of-squares (n)
   "Calculates the sum of squares for a given number."
-  )
+  (reduce #'+ (mapcar #'(lambda(i) (expt i 2)) (loop for i from 0 to n collect i))))
 
 (defun difference (n)
   "Finds the diff. between the square of the sum and the sum of the squares."
-  )
+  (- (square-of-sum n) (sum-of-squares n)))
+
+
+;(format t "output: ~D~%" (square-of-sum 10))
+  
