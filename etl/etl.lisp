0e573ddc91c064ec1c4b7bfebd150a8ef0f45e60
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-10-04 23:23:29 +0100

    Cleaning up

--- etl/etl.lisp
+++ etl/etl.lisp
@@ -4,16 +4,6 @@
 
 (in-package :etl)
 
-(defun hash-table-from-alist (kvs)
-  "Test helper method to create a hash table from a list of key-value pairs."
-  (reduce #'(lambda (h kv) (setf (gethash (car kv) h) (cdr kv)) h)
-          kvs
-          :initial-value (make-hash-table)))
-(defun prnt (has-table)
-
-  (loop for key being the hash-keys in has-table do
-        (format t "Key: ~A, Value: ~A~%" key (gethash key has-table))))
-
 (defun transform (data)
   "Transforms hash values into keys with their keys as their values."
   (let
@@ -26,7 +16,3 @@
                        (format t "Looping for key: ~A, value: ~A~%" (car hash-item) value)
                        (setf (gethash (char (string-downcase value) 0) my-hash-table) key))))
     my-hash-table))
-
-(format t "~A~%" (prnt (transform (hash-table-from-alist '((1 . (#\A #\E)) (2 . (#\D #\G)))))))
-
-
