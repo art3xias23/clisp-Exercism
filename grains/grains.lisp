b584014c4f3e8d6a72b4686093d126e65f450dfa
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-10-04 23:37:48 +0100

    Finished grains solution

--- grains/grains.lisp
+++ grains/grains.lisp
@@ -3,6 +3,10 @@
   (:export :square :total))
 (in-package :grains)
 
-(defun square (n) )
+(defun square (n)
+  (expt 2 (1- n)))
 
-(defun total () )
+(defun total ()
+  (reduce #'+ (mapcar #'(lambda(x) (square x)) (loop for i from 1 to 64 collect i))))
+
+(format t "Answer: ~D~%" (square 3))
