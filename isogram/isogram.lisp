49e6e5a0f302262b075ca1a31860251e73ff9342
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-10-05 18:49:44 +0100

    Finished isogram

--- isogram/isogram.lisp
+++ isogram/isogram.lisp
@@ -4,15 +4,17 @@
 
 (in-package :isogram)
 
-(defun isogram-p (string)
+(defun isogram-p (strng)
   "Is string an Isogram?"
         (let ((my-hash-table (make-hash-table))
-              (str (replace (replace string "-") " ")))
+              (str (remove #\-
+                     (remove #\Space 
+                       (string-downcase strng)))))
           (loop for ch across str do
                 (let ((letter (gethash ch my-hash-table)))
                   (if letter
-                      (return-from isogram-p NIL)
+                      (progn
+                        (return-from isogram-p NIL))
                       (setf (gethash ch my-hash-table) 1)))))
   T)
-                    
-                  
+
