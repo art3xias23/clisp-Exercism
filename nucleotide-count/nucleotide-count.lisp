f0118fa54c5b5aec3ce70a3af50ff31297b6ab98
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-10-08 23:25:14 +0100

    Added initial dna count

--- nucleotide-count/nucleotide-count.lisp
+++ nucleotide-count/nucleotide-count.lisp
@@ -6,8 +6,12 @@
 
 (defun dna-count (nucleotide strand)
   "Returns a count of the given nucleotide appearing in a DNA strand."
-  )
+  (reduce (lambda (cc ch)
+              (if (char= ch nucleotide)
+                  (setq cc (1+ cc))
+                  cc)) strand :initial-value 0)
+  
 
-(defun nucleotide-counts (strand)
-  "Returns a hash of nucleotides and their counts in a given DNA strand."
-  )
+ (defun nucleotide-counts (strand)
+   "Returns a hash of nucleotides and their counts in a given DNA strand."))
+  
