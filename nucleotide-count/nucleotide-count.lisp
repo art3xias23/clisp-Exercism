02fa40d345f240d8901f014c48edcafc6c37b2ea
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-10-08 23:40:32 +0100

    Finished nucleotide counts

--- nucleotide-count/nucleotide-count.lisp
+++ nucleotide-count/nucleotide-count.lisp
@@ -9,9 +9,23 @@
   (reduce (lambda (cc ch)
               (if (char= ch nucleotide)
                   (setq cc (1+ cc))
-                  cc)) strand :initial-value 0)
+                  cc)) strand :initial-value 0))
   
 
- (defun nucleotide-counts (strand)
-   "Returns a hash of nucleotides and their counts in a given DNA strand."))
+(defun nucleotide-counts (strand)
+  (let ((my-hash-table (make-hash-table)))
+    (map nil (lambda (ch)
+               (if (gethash ch my-hash-table)
+                   (incf (gethash ch my-hash-table))
+                   (setf (gethash ch my-hash-table) 1))) strand)
+    my-hash-table))
+
+(defun print-hash-table (my-hash-table)
+  "Returns a hash of nucleotides and their counts in a given DNA strand."
+  (format t "Printing my hash table~%")
+  (loop for key being each hash-key in my-hash-table do
+        (format t "Key: ~A, Value: ~A~%" key (gethash key my-hash-table))))
+
+(print-hash-table (nucleotide-counts "CACDGHA"))
+
   
