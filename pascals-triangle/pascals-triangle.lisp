6bfa913bc7bdaf974c5510ebdb569496bce98418
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-10-12 22:46:18 +0100

    Added basic pyramid solution

--- pascals-triangle/pascals-triangle.lisp
+++ pascals-triangle/pascals-triangle.lisp
@@ -3,6 +3,30 @@
   (:export :rows))
 (in-package :pascals-triangle)
 
-(defun rows (n))
+;(defun rows (n))
 
-(defun pyramid (n))
+(defun pyramid (n)
+  (let (
+        (y (1- (* 2 n)))
+        (x n))
+    (loop for i from 1 to (1+ x) do
+          (let (
+                (diff (- n i)))
+            (loop for j from 1 to (1+ y) do
+                                      (if (and (> j diff) (< j (- y diff)))
+                                          (if (not (= (mod i 2) 0))
+                                              (if (not (= (mod j 2) 0))
+                                                  (format t "x")
+                                                  (format t " "))
+                                              (if (= (mod j 2) 0)
+                                                  (format t "x")
+                                                  (format t " ")))
+                                          (format t " "))))
+          (format t "~%"))))
+
+
+(pyramid 4)
+
+                 
+
+          
